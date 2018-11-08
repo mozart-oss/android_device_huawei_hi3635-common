ba34afa5f8a14f4c4c68772eb6cafecc55a3173f
Huawei-Dev <surdu_petru@yahoo.com>
2018-11-08 20:43:34 +0100

    mozart: roodir: Import core init scripts and services
    * Removed NFC, Camera & Thermal services
    * Added oeminfo_nvm_server and teecd from stock
    
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- device.mk
+++ device.mk
@@ -13,5 +13,20 @@ $(call inherit-product, vendor/huawei/mozart/mozart-vendor.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.hi3635 \
+    init.hi3635.chip.usb.rc \
+    init.hi3635.connectivity.rc \
+    init.hi3635.gps.rc \
+    init.hi3635.power.rc \
+    init.hi3635.rc \
+    init.hi3635.usb.rc \
+    ueventd.hi3635.rc
+
+PRODUCT_PACKAGES += \
+    oeminfo_nvm_server \
+    teecd
+
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
