cfaa388b5f2a9c7dc6213b2db1c32f3b5f549f36
schwienernitzel <pfelix0803@gmail.com>
2024-09-29 15:03:51 +0000

    mozart: overlay: Import `power_profile.xml` from Huawei
    * Also set config_showNavigationBar = true when we're at it
    
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.hi3635 \
