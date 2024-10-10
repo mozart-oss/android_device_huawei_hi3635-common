819863ce8fb404e0bd347d46c38989215b7cb6d7
schwienernitzel <pfelix0803@gmail.com>
2024-10-10 10:39:46 +0000

    mozart: libshims: Rework `libshim_gui` structure
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- libshims/gui.c
+++ libshims/gui.c
@@ -4,18 +4,16 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+/* SensorManager */
+int _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE() {
+    return 0;
+}
+
 int _ZN7android13SensorManager21getInstanceForPackageERKNS_8String16E() {
     return 0;
 }
 
-int _ZN7android14SurfaceControl25setBlurMaskAlphaThresholdEf() {
-    return 0;
-}
-
-int _ZN7android14SurfaceControl18setBlurMaskSurfaceERKNS_2spIS0_EE() {
-    return 0;
-}
-
+/* SurfaceControl */
 int _ZN7android14SurfaceControl7setBlurEf() {
     return 0;
 }
@@ -24,6 +22,10 @@ int _ZN7android14SurfaceControl19setBlurMaskSamplingEj() {
     return 0;
 }
 
-int _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE() {
+int _ZN7android14SurfaceControl25setBlurMaskAlphaThresholdEf() {
+    return 0;
+}
+
+int _ZN7android14SurfaceControl18setBlurMaskSurfaceERKNS_2spIS0_EE() {
     return 0;
 }
