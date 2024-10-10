8aca7a78257feee94e75eb658cf6961fed0c3c14
schwienernitzel <pfelix0803@gmail.com>
2024-10-10 10:34:58 +0000

    mozart: libshims: Rework `libshim_hisi` structure
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- libshims/shim.c
+++ libshims/shim.c
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#define LOG_TAG "libshim"
+#define LOG_TAG "libshim_hisi"
 
 #include <cutils/log.h>
 #include <stdint.h>
@@ -13,21 +13,38 @@
 #include <libexif/exif-entry.h>
 
 /* liblog */
-extern int __hi_log_print(){ return 0; }
-extern int __android_janklog_print() { return 0; }
-extern int __android_logPower_print() { return 0; }
-extern int __android_log_exception_write() { return 0; }
+extern int __hi_log_print() {
+    return 0;
+}
+
+extern int __android_janklog_print() {
+    return 0;
+}
+
+extern int __android_logPower_print() {
+    return 0;
+}
+
+extern int __android_log_exception_write() {
+    return 0;
+}
 
 /* libexif */
-extern void exif_entry_gps_initialize(ExifEntry * e, ExifTag tag)
-{
+extern void exif_entry_gps_initialize(ExifEntry * e, ExifTag tag) {
     ALOGD("ExifTag = %d", (int)tag);
     exif_entry_initialize(e,tag);
 }
 
 /* libcutils */
-extern void atrace_async_begin_body_HwCam(const char* name,int32_t cookie) {}
+extern void atrace_async_begin_body_HwCam(const char* name,int32_t cookie) {
+    // no-op, proprietary Huawei function
+}
 
 /* libaudio_route */
-extern int pop_seq_init(){return 0;};
-extern int pop_seq_set(){return 0;};
+extern int pop_seq_init() {
+    return 0;
+}
+
+extern int pop_seq_set() {
+    return 0;
+}
