9adae1ded57259a0951d420a17c615d29e286096
schwienernitzel <pfelix0803@gmail.com>
2024-10-16 20:45:02 +0000

    mozart: `BoardConfig.mk:` Switch to `kernel/huawei/hi3635` kernel path
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_HEADER_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_SOURCE := kernel/huawei/mozart
+TARGET_KERNEL_SOURCE := kernel/huawei/hi3635
 TARGET_KERNEL_CONFIG := mozart_defconfig
 
 # Lights
